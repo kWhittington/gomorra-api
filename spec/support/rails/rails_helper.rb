941519230b4bd20d4ea0517751969b40f33656d2
Kyle Whittington <kthomaswhittington@gmail.com>
2015-05-29 21:43:13 -0500

    Remove mass spec/support/ requirement from support/rails/rails_helper.rb

--- spec/support/rails/rails_helper.rb
+++ spec/support/rails/rails_helper.rb
@@ -1,17 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'support/spec_helper'
 require File.expand_path('../../../../config/environment', __FILE__)
 require 'rspec/rails'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+require 'support/spec_helper'
+require 'support/other/factory_girl'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
