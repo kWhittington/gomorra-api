d0a6726e7e75d2440f67259ff3212866267ba77f
Kyle Whittington <kthomaswhittington@gmail.com>
2015-06-11 21:25:40 -0500

    Add GomorraSerializer.serialize_collection and specs.

--- app/serializers/gomorra_serializer.rb
+++ app/serializers/gomorra_serializer.rb
@@ -3,4 +3,9 @@ require 'jsonapi-serializers'
 # The super class serializer all other serializer inherit from.
 class GomorraSerializer
   include JSONAPI::Serializer
+
+  def self.serialize_collection(collection, options)
+    options.merge!(is_collection: true)
+    serialize(collection, options)
+  end
 end
--- spec/serializers/gomorra_serializer_spec.rb
+++ spec/serializers/gomorra_serializer_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe GomorraSerializer, tags do
   it 'is a JSONAPI::Serializer' do
     is_expected.to be_a(JSONAPI::Serializer)
   end
+
+  describe '.serialize_collection(collection, options)' do
+    subject :serialize_collection do
+      described_class.serialize_collection(collection, options)
+    end
+
+    let(:collection) { instance_double(Array) }
+    let(:options) { instance_double(Hash).as_null_object }
+
+    it 'calls .serialize(collection, options) with the given collection' do
+      expect(described_class).to receive(:serialize)
+        .with(collection, anything)
+
+      serialize_collection
+    end
+
+    it 'calls .serialize(collection, options) with is_collection: true' do
+      expect(options).to receive(:merge!).with(is_collection: true)
+      expect(described_class).to receive(:serialize)
+        .with(anything, options)
+
+      serialize_collection
+    end
+  end
 end
