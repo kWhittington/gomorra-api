b10544b757d1c9430e22da7367b5f6ff1608e92d
Kyle Whittington <kthomaswhittington@gmail.com>
2015-06-15 22:11:38 -0500

    Add Suit has many playing cards association.

--- app/models/suit.rb
+++ app/models/suit.rb
@@ -1,4 +1,6 @@
 # A standard 52-count bicycle deck suit of cards.
 class Suit < ActiveRecord::Base
   attr_readonly :name
+
+  has_many :playing_cards
 end
--- spec/models/suit_spec.rb
+++ spec/models/suit_spec.rb
@@ -5,6 +5,10 @@ tags = {
 }
 
 RSpec.describe Suit, tags do
+  it 'has many playing cards' do
+    is_expected.to have_many(:playing_cards)
+  end
+
   it 'has readonly name' do
     is_expected.to have_readonly_attribute(:name)
   end
