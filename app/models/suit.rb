4fe11f603c959395730991702bdb76d9aa54fd2a
Kyle Whittington <kthomaswhittington@gmail.com>
2015-07-18 15:44:53 -0500

    Add Suit::NAMES specs.

--- app/models/suit.rb
+++ app/models/suit.rb
@@ -1,6 +1,24 @@
 # A standard 52-count bicycle deck suit of cards.
-class Suit < ActiveRecord::Base
-  attr_readonly :name
+class Suit
+  NAMES = {
+    clubs: 'club',
+    diamonds: 'diamond',
+    hearts: 'heart',
+    spades: 'spade'
+  }
 
-  has_many :playing_cards
+  def self.of(pluralized_name)
+    unless SUIT_NAMES.key?(pluralized_name)
+      fail 'Suit of #{pluralized_name} does not exist'
+    end
+
+    new(name: SUIT_NAMES[pluralized_name])
+  end
+
+  attr_reader :name
+
+  def initialize(name:)
+    fail 'Cannot create Suit without name' if name.blank?
+    @name = name
+  end
 end
--- spec/models/suit_spec.rb
+++ spec/models/suit_spec.rb
@@ -5,6 +5,30 @@ tags = {
 }
 
 RSpec.describe Suit, tags do
+  describe '::NAMES' do
+    subject(:names) { described_class::NAMES }
+
+    it 'has exactly four names' do
+      is_expected.to have_exactly(4).names
+    end
+
+    it 'maps :clubs to `club`' do
+      is_expected.to include(clubs: 'club')
+    end
+
+    it 'maps :diamonds to `diamond`' do
+      is_expected.to include(diamonds: 'diamond')
+    end
+
+    it 'maps :hearts to `heart`' do
+      is_expected.to include(hearts: 'heart')
+    end
+
+    it 'maps :spades to `spade' do
+      is_expected.to include(spades: 'spade')
+    end
+  end
+
   subject(:suit) { described_class.new(name: name) }
   let(:name) { Faker::Lorem.word }
 
