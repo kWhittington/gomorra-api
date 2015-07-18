8283e4b04bd534211a1242d771d7fb51cbbb56aa
Kyle Whittington <kthomaswhittington@gmail.com>
2015-07-18 14:35:25 -0500

    Remove Suit::NAMES spec.
    This reverts commit 62b1cb052948a1ddf7eb60660165102181c21926.

--- app/models/suit.rb
+++ app/models/suit.rb
@@ -1,7 +1,5 @@
 # A standard 52-count bicycle deck suit of cards.
 class Suit < ActiveRecord::Base
-  NAMES = %w(club diamond heart spade)
-
   attr_readonly :name
 
   has_many :playing_cards
--- spec/models/suit_spec.rb
+++ spec/models/suit_spec.rb
@@ -5,30 +5,6 @@ tags = {
 }
 
 RSpec.describe Suit, tags do
-  describe '::NAMES' do
-    subject(:names) { described_class::NAMES }
-
-    it 'has exactly four elements' do
-      is_expected.to have_exactly(4).items
-    end
-
-    it 'includes `club`' do
-      is_expected.to include('club')
-    end
-
-    it 'includes `diamond`' do
-      is_expected.to include('diamond')
-    end
-
-    it 'includes `heart`' do
-      is_expected.to include('heart')
-    end
-
-    it 'includes `spade`' do
-      is_expected.to include('spade')
-    end
-  end
-
   it 'has many playing cards' do
     is_expected.to have_many(:playing_cards)
   end
