9b029dd2dad390b2f13c8ac6e0a553324f463316
Kyle Whittington <kthomaswhittington@gmail.com>
2015-08-04 10:59:46 -0500

    Reformat PlayingCard specs.

--- spec/models/playing_card_spec.rb
+++ spec/models/playing_card_spec.rb
@@ -8,6 +8,20 @@ tags = {
 RSpec.describe PlayingCard, tags do
   subject(:playing_card) { described_class.new }
 
+  it 'belongs to a rank' do
+    is_expected.to belong_to(:rank)
+  end
+
+  it 'belongs to a suit' do
+    is_expected.to belong_to(:suit)
+  end
+
+  it 'validates uniqueness of rank and suit' do
+    is_expected.to validate_uniqueness_of(:rank_id)
+      .with_message('there can only be one rank of each suit')
+      .scoped_to(:suit_id)
+  end
+
   describe '.exists_with_rank(name)' do
     subject(:exists_with_rank) { described_class.exists_with_rank(name) }
 
@@ -26,19 +40,5 @@ RSpec.describe PlayingCard, tags do
         is_expected.to be_falsey
       end
     end
-  end
-
-  it 'belongs to a rank' do
-    is_expected.to belong_to(:rank)
-  end
-
-  it 'belongs to a suit' do
-    is_expected.to belong_to(:suit)
-  end
-
-  it 'validates uniqueness of rank and suit' do
-    is_expected.to validate_uniqueness_of(:rank_id)
-      .with_message('there can only be one rank of each suit')
-      .scoped_to(:suit_id)
   end
 end
