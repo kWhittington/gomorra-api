318b33d1e296524a731da55bb61972eda666a271
Kyle Whittington <kthomaswhittington@gmail.com>
2015-08-04 10:58:29 -0500

    Reformat playing_card.rb

--- app/models/playing_card.rb
+++ app/models/playing_card.rb
@@ -4,10 +4,10 @@ class PlayingCard < ActiveRecord::Base
   belongs_to :suit
 
   validates :rank_id,
-    uniqueness: {
-      message: 'there can only be one rank of each suit',
-      scope: :suit_id
-    }
+            uniqueness: {
+              message: 'there can only be one rank of each suit',
+              scope: :suit_id
+            }
 
   validates_associated :rank
   validates_associated :suit
